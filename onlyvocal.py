0499c0e4e0fcef5fc5fd326ced2abd0652949ecf
yuyuyzl <yuzhongliang22@163.COM>
2020-02-02 04:36:52 +0800

    multiprocess pool applied

--- onlyvocal.py
+++ onlyvocal.py
@@ -1,52 +1,65 @@
 from pydub import AudioSegment
+from multiprocessing import Pool
 import math
+def calculateMinK(item):
+    songSlice = item[0]
+    musicSlice = item[1]
+    mindbfs = 1
+    for k in range(0, 200):
+        dbfs = songSlice.overlay(musicSlice.apply_gain(-k * 0.1)).dBFS
+        if dbfs < mindbfs:
+            mink = k
+            mindbfs = dbfs
+    return -mink * 0.1
 
-sliceLength=100
-song = AudioSegment.from_wav("lion518vocalEQ.wav")
-music = AudioSegment.from_wav("lion518instEQ.wav")
-totalSlices=math.ceil(len(song)/sliceLength)
-song=song[::sliceLength]
-music=music[::sliceLength]
+def applyGain(item):
+    songSlice = item[0]
+    musicSlice = item[1]
+    gain = item[2]
+    return songSlice.overlay(musicSlice.apply_gain(gain))
 
-#mix = song.overlay(music.apply_gain(-8.4))
+if __name__ == '__main__':
+    sliceLength=100
+    song = AudioSegment.from_wav("lion518vocalEQ.wav")
+    music = AudioSegment.from_wav("lion518instEQ.wav")
+    totalSlices=math.ceil(len(song)/sliceLength)
+    song=song[::sliceLength]
+    music=music[::sliceLength]
+
+    #mix = song.overlay(music.apply_gain(-8.4))
 
 
-mix=AudioSegment.empty()
+    mix=AudioSegment.empty()
 
-currentSlice=0
-mink=0
-kList=[]
-for item in zip(song,music):
-    currentSlice+=1
-    songSlice=item[0]
-    musicSlice=item[1]
-    mindbfs=1
-    for k in range(70,200):
-        dbfs=songSlice.overlay(musicSlice.apply_gain(-k*0.1)).dBFS
-        if dbfs<mindbfs:
-            mink=k
-            mindbfs=dbfs
-    print((currentSlice/totalSlices),-mink*0.1)
-    kList.append(-mink*0.1)
-    #mix+=songSlice.overlay(musicSlice.apply_gain(-mink*0.1))
+    currentSlice=0
+    mink=0
+    kList=[]
 
-currentSlice=0
-fadeDuration=20
-song = AudioSegment.from_wav("lion518vocalEQ.wav")
-music = AudioSegment.from_wav("lion518instEQ.wav")
-song=song[::sliceLength]
-music=music[::sliceLength]
-for item in zip(song,music):
-    songSlice=item[0]
-    musicSlice=item[1]
-    iStart=currentSlice-1
-    if(iStart<0):
-        iStart=0
-    iEnd=currentSlice+1
-    arr=kList[iStart:iEnd]
-    gain=sum(arr)/len(arr)
-    mixSlice=songSlice.overlay(musicSlice.apply_gain(gain))
-    mix+=mixSlice
-    print((currentSlice/totalSlices))
-    currentSlice+=1
-mix.export("export.wav", format="wav")
+
+    with Pool() as p:
+        kList=p.map(calculateMinK,zip(song,music),totalSlices//64)
+
+
+    fadeDuration=20
+    song = AudioSegment.from_wav("lion518vocalEQ.wav")
+    music = AudioSegment.from_wav("lion518instEQ.wav")
+    song=song[::sliceLength]
+    music=music[::sliceLength]
+    targetGain=[]
+    for currentSlice in range(len(kList)):
+        iStart = currentSlice - 1
+        if (iStart < 0):
+            iStart = 0
+        iEnd = currentSlice + 1
+        arr = kList[iStart:iEnd]
+        gain = sum(arr) / len(arr)
+        targetGain.append(gain)
+
+
+    with Pool() as p:
+        mixSlices=p.map(applyGain,zip(song,music,targetGain),totalSlices//64)
+
+    for s in mixSlices:
+        mix+=s
+
+    mix.export("export.wav", format="wav")
