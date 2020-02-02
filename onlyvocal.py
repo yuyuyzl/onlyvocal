6a69cbe841abc284e2ea91a8647fc01d0ef219d1
yuyuyzl <yuzhongliang22@163.COM>
2020-02-02 19:28:55 +0800

    进度条

--- onlyvocal.py
+++ onlyvocal.py
@@ -1,6 +1,7 @@
 from pydub import AudioSegment
 from multiprocessing import Pool
 import math
+import time
 
 
 def calculateMinK(item):
@@ -21,54 +22,63 @@ def applyGain(item):
     gain = item[2]
     return songSlice.overlay(musicSlice.apply_gain(gain))
 
+
 def appendSlices(item):
-    mix=AudioSegment.empty()
+    mix = AudioSegment.empty()
     for s in item:
-        mix+=s
+        mix += s
     return mix
 
 
+def showProgress(asyncList, name="Task"):
+    totalProgress = asyncList._number_left
+    while not asyncList.ready():
+        time.sleep(2)
+        print(name, "%.1f%%" % (100 - asyncList._number_left * 100 / totalProgress))
+    return asyncList.get()
+
+
 if __name__ == '__main__':
-    songPath="いのちの名前rec.wav"
-    musicPath="いのちの名前inst.wav"
+    songPath = "いのちの名前rec.wav"
+    musicPath = "いのちの名前inst.wav"
     with Pool() as p:
         print("START")
-        sliceLength=10
+        sliceLength = 10
         song = AudioSegment.from_wav(songPath)
         music = AudioSegment.from_wav(musicPath)
-        totalSlices=math.ceil(len(song)/sliceLength)
-        song=song[::sliceLength]
-        music=music[::sliceLength]
+        totalSlices = math.ceil(len(song) / sliceLength)
+        song = song[::sliceLength]
+        music = music[::sliceLength]
 
-        mix=AudioSegment.empty()
+        mix = AudioSegment.empty()
 
-        currentSlice=0
-        mink=0
+        currentSlice = 0
+        mink = 0
 
         print("CALCULATE MIN K")
-        kList=p.map(calculateMinK,zip(song,music),totalSlices//64)
+        kList = showProgress(p.map_async(calculateMinK, zip(song, music), totalSlices // 99),"MINK")
 
         song = AudioSegment.from_wav(songPath)
         music = AudioSegment.from_wav(musicPath)
-        song=song[::sliceLength]
-        music=music[::sliceLength]
-        targetGain=[]
+        song = song[::sliceLength]
+        music = music[::sliceLength]
+        targetGain = []
         for currentSlice in range(len(kList)):
-            iStart = currentSlice - 1
+            iStart = currentSlice - 5
             if iStart < 0:
                 iStart = 0
-            iEnd = currentSlice + 2
+            iEnd = currentSlice + 6
             arr = kList[iStart:iEnd]
             gain = sum(arr) / len(arr)
             targetGain.append(gain)
 
         print("APPLY GAIN")
-        mixSlices=p.map(applyGain,zip(song,music,targetGain),totalSlices//64)
-        while len(mixSlices)>1:
-            print("MIX SLICES",len(mixSlices))
-            mixSlices=p.map(appendSlices, [mixSlices[i:i+2] for i in range(0, len(mixSlices), 2)], 8)
+        mixSlices = showProgress(p.map_async(applyGain, zip(song, music, targetGain), totalSlices // 99),"GAIN")
+        while len(mixSlices) > 1:
+            print("MIX SLICES", len(mixSlices))
+            mixSlices = p.map(appendSlices, [mixSlices[i:i + 2] for i in range(0, len(mixSlices), 2)], 8)
 
-        mix=mixSlices[0]
+        mix = mixSlices[0]
         # if len(mix)<25 or len(s)<25:
         #     mix+=s
         # else:
